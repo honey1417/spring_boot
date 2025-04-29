da95808a171b01deb054c063ef85efb0235bcffb
harshini <harshinipamarthi@gmail.com>
2025-04-29 13:00:53 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -27,15 +27,15 @@ provider "helm" {
 module "delegate" {
   source = "harness/harness-delegate/kubernetes"
   version = "0.1.8"
-
-  account_id = "ucHySz2jQKKWQweZdXyCog"
-  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
-  delegate_name = "terraform-delegate"
+  account_id = "TnI9iZ6IRnCmpxjiN2xMGg"
+  delegate_token = "YzJjNmU3MmYxNjZmM2ViYjZhMmQ2MzBlNGJjNDhhOGM="
+  delegate_name = "terraform-delegate-h"
   deploy_mode = "KUBERNETES"
   namespace = "harness-delegate-ng"
   manager_endpoint = "https://app.harness.io"
-  delegate_image = "harness/delegate:25.02.85300"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.04.85602"
   replicas = 1
   upgrader_enabled = true
+
   depends_on       = [google_container_cluster.primary]
 }
